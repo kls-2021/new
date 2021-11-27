24d7822dad22b46ff05c27cb583236da1fe9f40b
kls-2021 <90022844+kls-2021@users.noreply.github.com>
2021-11-27 16:41:58 +0530

    Add files via upload

---
+++ Task_1.js
@@ -0,0 +1,30 @@
+console.log("creating an object person.It has 3 properties: firstNname, lastName, officeAddress.");
+console.log("");
+var person=new Object();
+
+person.firstNname="Lakshmi";
+person.lastName="Surekha";
+person.officeAddress="Rajamundry";
+console.log("properties of person 1:");
+console.log("firstNname: "+person.firstNname);
+console.log("lastName: "+person.lastName);
+console.log("officeAddress: "+person.officeAddress);
+console.log("");
+
+person.firstNname="Vaishnavi";
+person.lastName="vinnakota";
+person.officeAddress="Vishakapatnam";
+console.log("properties of person 2:");
+console.log("firstNname: "+person.firstNname);
+console.log("lastName: "+person.lastName);
+console.log("officeAddress: "+person.officeAddress);
+console.log("");
+
+person.firstNname="Priya";
+person.lastName="Varshini";
+person.officeAddress="Telangana";
+console.log("properties of person 3:");
+console.log("firstNname: "+person.firstNname);
+console.log("lastName: "+person.lastName);
+console.log("officeAddress: "+person.officeAddress);
+console.log("");
